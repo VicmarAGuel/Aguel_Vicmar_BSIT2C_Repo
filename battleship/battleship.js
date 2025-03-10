937e8ca4f0b52cd4a364db0eb5b1e65c39cbd531
VicmarAGuel <avicmar@filamer.edu.ph>
2025-03-10 16:19:41 +0800

    battleship_v1

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,1 +1,36 @@
-console.log("battleship game start")
+// declaring variable
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+// Game logic
+
+while (isSunk == false){
+    guess = prompt("Ready!. Aim!. Fire! (enter number 0-6)");
+    if (guess < 0 || guess > 6){
+        alert("Please enter a valid cell number!");
+    }else {
+        guesses = guesses + 1;
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1;
+            alert ("hits")
+            if (hits == 3) {
+                inSunk = true;
+                alert("You sank my battleship!")
+            }
+        }
+        else{
+            alert("Miss!")
+        }
+    }
+}
+
+var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is: " + (3/guesses);
+
+alert(stats);
